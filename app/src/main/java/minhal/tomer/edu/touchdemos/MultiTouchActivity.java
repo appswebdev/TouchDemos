319eb12425ee162cc848a29716fafaaf9635dbf3
appswebdev <appswebdev@gmail.com>
2016-11-16 18:41:50 +0200

    MultiTouch View Draw Multiple Fingers

--- app/src/main/java/minhal/tomer/edu/touchdemos/MultiTouchActivity.java
+++ app/src/main/java/minhal/tomer/edu/touchdemos/MultiTouchActivity.java
@@ -16,47 +16,6 @@ public class MultiTouchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_multi_touch);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent e) {
-        int actionMasked = e.getActionMasked();
-        int pointerCount = e.getPointerCount();
-        int index = e.getActionIndex();
-        int pointerId = e.getPointerId(index);
-
-        switch (actionMasked){
-            case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_POINTER_DOWN:
-                //Log.d(TAG, "Down");
-                Log.d(TAG, String.format("Down index = %d id = %d", index, pointerId));
-
-                break;
-
-            case MotionEvent.ACTION_MOVE:
-                Log.d(TAG, "Move");
-                break;
-
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_POINTER_UP:
-            case MotionEvent.ACTION_CANCEL:
-                //Log.d(TAG, "Up");
-                Log.d(TAG, String.format("Up index = %d id = %d", index, pointerId));
-                break;
-        }
-
-        return true;
+        setContentView(new MultiTouchView(this));
     }
 }
--- app/src/main/java/minhal/tomer/edu/touchdemos/MultiTouchView.java
+++ app/src/main/java/minhal/tomer/edu/touchdemos/MultiTouchView.java
@@ -4,9 +4,14 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by ANDROID on 16/11/2016.
@@ -15,10 +20,11 @@ public class MultiTouchView extends View {
 
 
     private static final float RADIUS = 30;
-    float pointerX;
-    float pointerY;
+
     Paint mPaint;
     private String TAG = "Minhal";
+    HashMap<Integer, PointF> mPointers = new HashMap<>();
+
 
     public MultiTouchView(Context context) {
         super(context);
@@ -38,11 +44,14 @@ public class MultiTouchView extends View {
         int pointerId = e.getPointerId(index);
 
 
-        pointerX = e.getX(0);
-        pointerY = e.getY(0);
-        switch (actionMasked){
+        float x = e.getX(index);
+        float y = e.getY(index);
+
+        switch (actionMasked) {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_POINTER_DOWN:
+                PointF p = new PointF(x, y);
+                mPointers.put(pointerId, p);
                 //Log.d(TAG, "Down");
                 Log.d(TAG, String.format("Down index = %d id = %d", index, pointerId));
 
@@ -50,12 +59,20 @@ public class MultiTouchView extends View {
 
             case MotionEvent.ACTION_MOVE:
                 Log.d(TAG, "Move");
+                for (int pointerIndex = 0; pointerIndex < pointerCount; pointerIndex++) {
+                    int id = e.getPointerId(pointerIndex);
+                    PointF currentPointer = mPointers.get(id);
+                    currentPointer.x = e.getX(pointerIndex);
+                    currentPointer.y = e.getY(pointerIndex);
+                    //currentPointer.set(e.getX(pointerIndex), e.getY(pointerIndex));
+                }
                 break;
 
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_POINTER_UP:
             case MotionEvent.ACTION_CANCEL:
                 //Log.d(TAG, "Up");
+                mPointers.remove(pointerId);
                 Log.d(TAG, String.format("Up index = %d id = %d", index, pointerId));
                 break;
         }
@@ -66,6 +83,11 @@ public class MultiTouchView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(pointerX, pointerY, RADIUS, mPaint);
+                for (Integer key : mPointers.keySet()) {
+            PointF p = mPointers.get(key);
+            if (p != null) {
+                canvas.drawCircle(p.x, p.y, RADIUS, mPaint);
+            }
+        }
     }
 }
